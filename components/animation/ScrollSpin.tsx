d453991a3cb661ccb780fe21df4cf26f215981f3
Karan Chaudhary <tharukaran930@gmail.com>
2023-10-06 02:10:58 +0545

    udpate final merge

--- components/animation/ScrollSpin.tsx
+++ components/animation/ScrollSpin.tsx
@@ -18,15 +18,9 @@ export const ScrollSpin = () => {
     }
   });
   return (
-<<<<<<< HEAD
-    <div className="fixed top-[85%] left-[2%]">
-      <motion.div
-        className="sticky  w-[100px] h-[100px] rounded-full"
-=======
     <div className="fixed z-[99] top-[85%] left-[2%]">
       <motion.div
         className="sticky w-[100px] h-[100px] rounded-full"
->>>>>>> code
         animate={{ rotate: deg }}
       >
         {/* <Link href="/" className="w-full h-full"></Link> */}
@@ -44,11 +38,7 @@ export const ScrollSpin = () => {
             </span>
           ))}
         </div>
-<<<<<<< HEAD
-        <div className="z-10 w-4 h-4 bg-slate-200 absolute top-[50%] transform -translate-y-[50%] left-[50%] -translate-x-[50%]"></div>
-=======
         <div className=" w-4 h-4 bg-slate-200 absolute top-[50%] transform -translate-y-[50%] left-[50%] -translate-x-[50%]"></div>
->>>>>>> code
       </motion.div>
     </div>
   );
